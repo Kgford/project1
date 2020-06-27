1925f8fddb08d61b0bacbe07bc72672b041ff52e
Kgford <39444453+Kgford@users.noreply.github.com>
2020-06-27 19:01:22 -0400

    saving parse debug

--- static/assets/js/book.js
+++ static/assets/js/book.js
@@ -10,45 +10,45 @@ document.addEventListener('DOMContentLoaded', () => {
 			// Callback function for when request is completed
 			request.onload = () =>{
 				const res = JSON.parse(request.responseText)
+				alert(res.book_list[0].id)
+				alert(res.book_list[0].title)
+				alert(res.book_list[0].author)
+				alert(res.book_list[0].year)
 				indata = JSON.stringify(res)
 				_book = JSON.parse(indata)
-				sucs = JSON.stringify(_book["success"])
-				sucs = JSON.parse(sucs)
-				alert(sucs)
-				_book = JSON.stringify(_book["book_list"])
-				_book = JSON.parse(_book)
-				alert(_book)
-							
+				alert(sucs = JSON.stringify(_book["success"]))
 				
+				
+					
 				// Update the result div
 				if (sucs) {
 					// Load the entire booklist
-					for (active_book in _book) { 
-					    alert(active_book)
+					for (active_book in res.book_list) { 
+					    alert(active_book.title)
 						var a1 = document.createElement('a');
-						a1.href = url_for('book', book_id=_book.id);
-						var node1 = document.createTextNode(_book.isbn);
+						a1.href = url_for('book', book_id=active_book.id);
+						var node1 = document.createTextNode(active_book.isbn);
 						a1.appendChild(node1);
 						var element1 = document.getElementById("b_isbn");
 						element1.appendChild(a1);
 						
 						var a2 = document.createElement('a');
-						a2.href = url_for('book', book_id=_book.id);
-						var node2 = document.createTextNode(_book.title);
+						a2.href = url_for('book', book_id=active_book.id);
+						var node2 = document.createTextNode(active_book.title);
 						a2.appendChild(node2);
 						var element2 = document.getElementById("b_title");
 						element2.appendChild(a2);
 						
 						var a3 = document.createElement('a');
-						a3.href = url_for('book', book_id=_book.id);
-						var node3 = document.createTextNode(_book.author);
+						a3.href = url_for('book', book_id=active_book.id);
+						var node3 = document.createTextNode(active_book.author);
 						a3.appendChild(node3);
 						var element3 = document.getElementById("b_author");
 						element3.appendChild(a3);
 						
 						var a4 = document.createElement('a');
-						a4.href = url_for('book', book_id=_book.id);
-						var node4 = document.createTextNode(_book.year);
+						a4.href = url_for('book', book_id=active_book.id);
+						var node4 = document.createTextNode(active_book.year);
 						a4.appendChild(node4);
 						var element4 = document.getElementById("b_year");
 						element4.appendChild(a4);		
